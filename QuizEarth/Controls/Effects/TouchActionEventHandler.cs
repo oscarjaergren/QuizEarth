2c2414c74e945692440680511cbd8777deb0f055
Oscar Jargren <Oscar.Jargren@mcdermottgroup.co.uk>
2020-02-27 12:45:21 +0000

    Now working map page

--- QuizEarth/Controls/Effects/TouchActionEventHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace QuizEarth.Controls.Effects
-{
-    public delegate void TouchActionEventHandler(object sender, TouchActionEventArgs args);
-}
